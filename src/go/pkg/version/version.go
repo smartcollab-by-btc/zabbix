e8af8368d9b024900498f101c5a8927ac78746ab
Continuous Integration <ci@zabbix.com>
2025-12-17 12:36:07 +0200

    .......... [ZBXNEXT-826] updated release date and version for 7.2.15

--- src/go/pkg/version/version.go
+++ src/go/pkg/version/version.go
@@ -23,11 +23,11 @@ import (
 
 //nolint:revive // these consts are part of higher level script
 const (
-	ZABBIX_REVDATE          = "16 December 2025"
+	ZABBIX_REVDATE          = "17 December 2025"
 	ZABBIX_VERSION_MAJOR    = 7
 	ZABBIX_VERSION_MINOR    = 2
 	ZABBIX_VERSION_PATCH    = 15
-	ZABBIX_VERSION_RC       = "rc2"
+	ZABBIX_VERSION_RC       = ""
 	ZABBIX_VERSION_RC_NUM   = "{ZABBIX_RC_NUM}"
 	ZABBIX_VERSION_REVISION = "{ZABBIX_REVISION}"
 	copyrightMessage        = "Copyright (C) 2025 Zabbix SIA\n" +
